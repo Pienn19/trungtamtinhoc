2e18968555a8bea26412051328db96917079bbfd
Doan Duy Hieu <doanhieu.11052004@gmail.com>
2026-06-06 12:55:41 +0700

    Fix/overall ver3 (#3)
    * fix: fix overall version 3
    
    * fix: fix conflict message for course

--- src/types/KhoaHoc.ts
+++ src/types/KhoaHoc.ts
@@ -56,6 +56,8 @@ export interface LopHocDTO {
     caHoc?: string
     phongHoc?: string
     hinhThucHoc?: string
+    isConflict?: boolean
+    conflictMessage?: string
 }
 
 export interface DangKyDTO {
